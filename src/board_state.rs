28c668568a700dc52d8c9d287fd2d025654b7db8
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-11-29 18:30:45 +0200

    added basic input logic

--- src/board_state.rs
+++ src/board_state.rs
@@ -1,1 +1,50 @@
-pub struct BoardState {}
+use std::fmt;
+
+pub struct BoardState {
+    state: Vec<Vec<char>>,
+    pub winner: Option<char>,
+}
+
+impl BoardState {
+    pub fn new() -> Self {
+        Self {
+            state: vec![vec![' '; 3]; 3],
+            winner: None,
+        }
+    }
+
+    pub fn make_move(&self, position: (usize, usize)) -> Self {
+        let mut new_state = self.state.clone();
+        new_state[position.0 - 1][position.1 - 1] = 'x';
+
+        Self {
+            winner: BoardState::check(&new_state),
+            state: new_state,
+        }
+    }
+
+    fn check(state: &Vec<Vec<char>>) -> Option<char> {
+        // TODO
+        if state[0][0] == 'x' && state[0][1] == 'x' && state[0][2] == 'x' {
+            return Some('X');
+        }
+
+        None
+    }
+}
+
+impl fmt::Display for BoardState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let state = self
+            .state
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .fold(String::new(), |acc, &c| format!("{} {}", acc, c))
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        write!(f, "{}", state)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
 #[warn(clippy::all)]
 mod board_state;
 
+use board_state::BoardState;
 use std::io;
 
 fn main() -> io::Result<()> {
+    let mut buffer: String;
+    let mut board = BoardState::new();
+
+    while board.winner.is_none() {
+        println!("{}\nYour turn", board);
+
+        buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        let player_move = parse_input(&buffer);
+
+        board = board.make_move(player_move);
+
+        println!("{}\nOpponent turn", board);
+    }
+
+    println!("Winner is: {}", board.winner.unwrap());
+
     Ok(())
+}
+
+fn extract_digit(string: &String, pos: usize) -> u32 {
+    string
+        .chars()
+        .nth(pos)
+        .expect("Invalid input")
+        .to_digit(10)
+        .expect("NaN")
+}
+
+fn parse_input(input: &String) -> (usize, usize) {
+    (
+        extract_digit(&input, 1) as usize,
+        extract_digit(&input, 3) as usize,
+    )
 }
